18d4c055e44d17b55779a757e96cc6e6b9db880e
oseiasfender-rgb <oseias.fender@gmail.com>
2026-06-26 15:14:02 -0300

    Delete sw.js

--- sw.js
+++
@@ -1,5 +0,0 @@
-const CACHE='oficinaos-v54730';
-const ASSETS=['./','./index.html','./oficinaos-v547-core.js','./clientes.html','./orcamento.html','./agenda.html','./financeiro.html','./relatorios.html','./metas.html','./ia.html','./manifest.webmanifest'];
-self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS).catch(()=>null)))});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
-self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))))});
